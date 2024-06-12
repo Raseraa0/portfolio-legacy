a2db18138ab12327aca60ab9f39a8aeeb796eda2
Arthur Rasera <arthur.rasera@grenoble.fr>
2024-06-12 09:05:14 +0200

    modif de style avec des fond dégradé etc

--- js/details.js
+++ js/details.js
@@ -5,6 +5,9 @@ export function setDetails() {
     let barreAfter = document.querySelector(".prenom-barre");
     barreAfter.style.borderRadius = `${barreAfter.clientHeight/2}px`; 
 
+    let barreMedia = document.querySelector(".social-media");
+    barreMedia.style.borderRadius = `${barreMedia.clientHeight/2}px`; 
+
     let projectsPlus = document.querySelectorAll(".project-plus");
     for(let projectPlus of projectsPlus){
         projectPlus.style.borderRadius = `${projectPlus.clientHeight/2}px`
