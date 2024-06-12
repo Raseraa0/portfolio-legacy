4d7f16ed58300c188120ed8bcb9bb7da3e154336
Arthur Rasera <arthur.rasera@grenoble.fr>
2024-06-12 17:15:18 +0200

    modif lien autre page

--- js/global.js
+++ js/global.js
@@ -1,20 +1,12 @@
 import { headerSetup } from './header.js';
 import { setDetails } from './details.js';
 
-import { redirectionWallpaperGenerator, } from './projects.js';
-import { redirectionGameOfLife } from './projects.js';
-import { redirectionBoidsSimulation } from './projects.js';
-
-
 
 
 function miseEnPlace() {
     headerSetup();
     setDetails();
 
-    document.querySelector(".project1 .project-plus").addEventListener('click', redirectionWallpaperGenerator);
-    document.querySelector(".project2 .project-plus").addEventListener('click', redirectionGameOfLife);
-    document.querySelector(".project3 .project-plus").addEventListener('click', redirectionBoidsSimulation);
 }
 
 window.addEventListener("load", miseEnPlace, false);
--- js/projects.js
+++
@@ -1,9 +0,0 @@
-export function redirectionWallpaperGenerator() {
-    window.location.href = "projects/wallpaper-generator/indexW.html";
-}
-export function redirectionGameOfLife() {
-    window.location.href = "projects/game-of-life/indexG.html";
-}
-export function redirectionBoidsSimulation() {
-    window.location.href = "projects/boids-simulation/indexB.html";
-}
